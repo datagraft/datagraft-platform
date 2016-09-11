bee96ef3811a5015bec83951a5b7ab8ff00481ee
Tarita Ana <annie.94@mail.ru>
2016-09-11 23:10:38 +0200

    Use test environment

--- startup.sh
+++ startup.sh
@@ -15,11 +15,11 @@ redirect_uri=$3
 
 docker exec -t datagraft-db bash -c 'until head -c 0 </dev/tcp/127.0.0.1/5432; do sleep 1; done'
 
-docker exec -t datagraft-db bash -c 'until psql -h localhost --user=postgres --list| grep -q datagraft-dev; do sleep 1; done'
+docker exec -t datagraft-db bash -c 'until psql -h localhost --user=postgres --list| grep -q datagraft-test; do sleep 1; done'
 
 docker exec datagraft-portal rake db:migrate
 
-docker exec -i datagraft-db psql --user=postgres --dbname=datagraft-dev -h localhost << EOF
+docker exec -i datagraft-db psql --user=postgres --dbname=datagraft-test -h localhost << EOF
 
 INSERT INTO users VALUES (1, 'administrator@datagraft.net', '\$2a\$10\$ArX10aADrz93gpptvrDkLuITB8O7s75IYnOjHBtCS/Tcp4OUJBwC2', null, null, null, 1, '2016-05-09 12:19:41.745909', '2016-05-09 12:19:41.745909', '10.0.2.2', '10.0.2.2', '2016-05-09 12:19:41.740838', '2016-05-09 12:19:41.747083', null, null, null, null, null, null, null, 'administrator', 0, true);
 
