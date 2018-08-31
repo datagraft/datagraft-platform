265e780d8b2c7cdb3932d600c12e35582c97bbcc
Brian Elvesæter <brian.elvesater@sintef.no>
2018-08-31 16:39:10 +0200

    Changed admin user to 'administrator@datagraft.io'

--- startup.sh
+++ startup.sh
@@ -21,7 +21,7 @@ docker exec datagraft-portal rake db:migrate
 
 docker exec -i datagraft-db psql --user=postgres --dbname=datagraft-dev -h localhost << EOF
 
-INSERT INTO users VALUES (1, 'administrator@datagraft.net', '\$2a\$10\$ArX10aADrz93gpptvrDkLuITB8O7s75IYnOjHBtCS/Tcp4OUJBwC2', null, null, null, 1, '2016-05-09 12:19:41.745909', '2016-05-09 12:19:41.745909', '10.0.2.2', '10.0.2.2', '2016-05-09 12:19:41.740838', '2016-05-09 12:19:41.747083', null, null, null, null, null, null, null, 'administrator', 0, true);
+INSERT INTO users VALUES (1, 'administrator@datagraft.io', '\$2a\$10\$ArX10aADrz93gpptvrDkLuITB8O7s75IYnOjHBtCS/Tcp4OUJBwC2', null, null, null, 1, '2016-05-09 12:19:41.745909', '2016-05-09 12:19:41.745909', '10.0.2.2', '10.0.2.2', '2016-05-09 12:19:41.740838', '2016-05-09 12:19:41.747083', null, null, null, null, null, null, null, 'administrator', 0, true);
 
 INSERT INTO oauth_applications (id, name, uid, secret, redirect_uri, scopes, created_at, updated_at) VALUES (1, 'Grafterizer', '$uid', '$secret', '$redirect_uri', 'public', '2016-05-04 08:44:01.702687', '2016-05-04 08:44:01.702687');
 EOF
